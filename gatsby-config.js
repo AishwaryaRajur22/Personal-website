ae8d938a45d837f436857626f85f4122162b0669
Aishwarya Rajur <aishwaryarajur@Aishwaryas-MacBook-Air.local>
2023-11-16 23:46:17 -0800

    changes 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Aishwarya Janardhana Rajur',
     description:
       'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
